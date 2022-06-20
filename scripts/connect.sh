920948066f6419384f73012e4cce7975d31557f8
Conn Warwicker <conn@cmrwarwicker.com>
2022-06-20 13:33:04 +0100

    Change connect script to use project type, instead of hard-coded 'web' container.

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -3,7 +3,7 @@
 # Require the project.sh so we can get the name of the web container.
 source $(pwd)'/env/scripts/project.sh'
 
-DEFAULT_CONTAINER=${PROJECT_NAME}'-web'
+DEFAULT_CONTAINER=${PROJECT_NAME}'-'${PROJECT_TYPE}
 CONTAINER_NAME=${1:-$DEFAULT_CONTAINER}
 
 docker exec -w / -it ${CONTAINER_NAME} bash
