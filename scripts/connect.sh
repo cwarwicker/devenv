52b587bbc78f17d45191ef160218fcaaa635d41f
Conn Warwicker <conn@cmrwarwicker.com>
2023-01-10 18:39:48 +0000

    Remove "/" working directory from script so it uses one from docker-compose

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -6,4 +6,4 @@ source $(pwd)'/env/scripts/project.sh'
 DEFAULT_CONTAINER=${PROJECT_NAME}'-'${PROJECT_TYPE}
 CONTAINER_NAME=${1:-$DEFAULT_CONTAINER}
 
-docker exec -w / -it ${CONTAINER_NAME} bash
+docker exec -it ${CONTAINER_NAME} bash
