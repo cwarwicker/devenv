680e4a9436a5211ac696f729663fcc54c49fca6f
Conn Warwicker <conn@cmrwarwicker.com>
2023-01-18 18:16:11 +0000

    Change backup script to take DB variables from project's .env file

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,17 +3,15 @@
 # Import the project.sh file to get all the variables.
 source $(pwd)'/env/scripts/project.sh'
 
-DAY=$(date +"%A")
+# Load the DB_* variables from .env file of project.
+set -o allexport
+source $(pwd)'/code/.env'
+set +o allexport
 
-# Database Backup Script.
-DB_USER=${1:-root}
-DB_PASS=${2:-password}
-FILE=${3:-"./backups/${PROJECT_NAME}-${DAY}.sql"}
-DB="${4:-main}"
+DAY=$(date +"%A")
+FILE=${1:-"./backups/${PROJECT_NAME}-${DAY}.sql"}
 CONTAINER="${PROJECT_NAME}-db"
 
-echo "[`date "+%d-%m-%Y %H:%M:%S"`] Initiating backup of [${DB}] to: [${FILE}]"
-
-docker exec -it ${CONTAINER} /usr/bin/mysqldump -u ${DB_USER} -p${DB_PASS} ${DB} > ${FILE}
-
+echo "[`date "+%d-%m-%Y %H:%M:%S"`] Initiating backup of [${DB_NAME}] to: [${FILE}]"
+docker exec -it ${CONTAINER} /usr/bin/mysqldump -u ${DB_USER} -p${DB_PASS} ${DB_NAME} > ${FILE}
 echo "[`date "+%d-%m-%Y %H:%M:%S"`] Backup complete"
